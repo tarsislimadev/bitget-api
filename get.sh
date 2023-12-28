fc09c1b0726e96fc29e034e78e5af81e32ca5783
Tarsis <br.tmvdl@gmail.com>
2023-12-28 11:54:35 -0300

    Updating app files

--- get.sh
+++ get.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/sh
 
-. .env
-
-. datetime.sh
-
 path="${1}"
 
 query="${2}"
 
+. .env
+
+. datetime.sh
+
+. passphrase.sh
+
 curl -sL -X GET "https://api.bitget.com/api/v2/${path}?${query}" \
   -H "ACCESS-KEY: ${ACCESS_KEY}" \
   -H "ACCESS-SIGN: ${ACCESS_SIGN}" \
-  -H "ACCESS-PASSPHRASE: ${ACCESS_PASSPHRASE}" \
+  -H "ACCESS-PASSPHRASE: ${PASSPHRASE}" \
   -H "ACCESS-TIMESTAMP: ${timestamp}" \
   -H "Locale: en-US" \
   -H "Content-Type: application/json"
---
+++ passphrase.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/sh
+
+PASSPHRASE="PASSPHRASE"
